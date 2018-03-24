90dca242393aa54f54aeaa26da5933dc64610641
Olle Lauri Boström <ollebostr@gmail.com>
2018-03-24 22:43:53 +0100

    Moving tests to jest

--- test/helloworld.test.js
+++ test/helloworld.test.js
@@ -1,35 +1,34 @@
-import chai, { expect } from 'chai';
 import helloWorld from '../src/helloworld';
 
 describe('{unit}: Testing helloWorld', () => {
   it('should return correct greeting in English', () => {
-    expect(helloWorld('English')).to.equal('Hello World');
+    expect(helloWorld('English')).toEqual('Hello World');
   });
   it('should return correct greeting in Spanish', () => {
-    expect(helloWorld('Spanish')).to.equal('Hola Mundo');
+    expect(helloWorld('Spanish')).toEqual('Hola Mundo');
   });
   it('should return correct greeting in French', () => {
-    expect(helloWorld('French')).to.equal('Bonjour le monde');
+    expect(helloWorld('French')).toEqual('Bonjour le monde');
   });
   it('should return correct greeting in Swedish', () => {
-    expect(helloWorld('Swedish')).to.equal('Hej världen');
+    expect(helloWorld('Swedish')).toEqual('Hej världen');
   });
   it('should return correct greeting in Chinese', () => {
-    expect(helloWorld('Chinese')).to.equal('你好，世界');
+    expect(helloWorld('Chinese')).toEqual('你好，世界');
   });
   it('should return correct greeting in Turkish', () => {
-    expect(helloWorld('Turkish')).to.equal('Merhaba Dünya');
+    expect(helloWorld('Turkish')).toEqual('Merhaba Dünya');
   });
   it('should default to English', () => {
-    expect(helloWorld()).to.equal('Hello World');
+    expect(helloWorld()).toEqual('Hello World');
   });
   it('should default be case insensitive', () => {
-    expect(helloWorld('swedish')).to.equal('Hej världen');
+    expect(helloWorld('swedish')).toEqual('Hej världen');
   });
   it('should throw a TypeError if argument is not a string', () => {
-    expect(() => helloWorld({})).to.throw(TypeError);
+    expect(() => helloWorld({})).toThrow(TypeError);
   });
   it('should handle handle unknown languages by returning message', () => {
-    expect(helloWorld('Parseltongue')).to.equal('Sorry, I don´t know how to say "Hello World" in Parseltongue yet.');
+    expect(helloWorld('Parseltongue')).toEqual('Sorry, I don´t know how to say "Hello World" in Parseltongue yet.');
   });
 });
